14cd361d36961a63337bd5002d735f1a18f53876
James Xue <jamesxue100@gmail.com>
2015-09-26 18:18:26 -0400

    Initial Commit

---
+++ Recogneyes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recogneyes
+//
+//  Created by James Xue on 9/26/15.
+//  Copyright © 2015 Rhodes and Xue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
